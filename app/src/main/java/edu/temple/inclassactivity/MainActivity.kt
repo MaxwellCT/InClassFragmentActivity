38c9827f9c3e286014f1c07f33124f0d77041123
Centrous <maxwellchanetekimang@gmail.com>
2025-03-13 12:16:08 -0400

    made changes and fix the fragment issue

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -1,19 +1,33 @@
-package edu.temple.inclassactivity
+    package edu.temple.inclassactivity
+    
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+    
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+    
+            // Fetch images into IntArray called imageArray
+            val typedArray = resources.obtainTypedArray(R.array.image_ids)
+            val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
+            typedArray.recycle()
+    
+            // Attach an instance of ImageDisplayFragment using factory method
+    
+            val ccf_image = intArrayOf(
+                R.drawable.ccf_saltedcaramel,
+                R.drawable.ccf_godiva,
+                R.drawable.ccf_coconutcreampie,
+                R.drawable.ccf_lowlicious
+            )
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Fetch images into IntArray called imageArray
-        val typedArray = resources.obtainTypedArray(R.array.image_ids)
-        val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
-        typedArray.recycle()
-
-        // Attach an instance of ImageDisplayFragment using factory method
-
+            val fragment = ImageDisplayFragment.newInstance(ccf_image)
+    
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView, fragment)
+                .commit()
+    
+        }
     }
-}
